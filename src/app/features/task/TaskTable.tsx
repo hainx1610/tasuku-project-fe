e066939bb6865c3479c8d1fb32c155ac23e853b4
Harris Nguyen <hainx1610@gmail.com>
2023-10-18 17:38:48 +0700

    chore: add loading screen for task table

--- src/app/features/task/TaskTable.tsx
+++ src/app/features/task/TaskTable.tsx
@@ -1,18 +1,26 @@
 import { columns } from "@/app/features/task/taskColumns";
+import LoadingScreen from "@/components/LoadingScreen";
 import { DataTable } from "@/components/ui/data-table";
 
-import { useSelector } from "react-redux";
+import { shallowEqual, useSelector } from "react-redux";
 
 // data = includeTasks arr
 export default function TaskTable() {
-  const { selectedProject } = useSelector((state: any) => state.project);
+  const { selectedProject, isLoading } = useSelector(
+    (state: any) => state.project,
+    shallowEqual
+  );
 
   return (
     <div className="container mx-auto py-10">
-      <DataTable
-        columns={columns}
-        data={selectedProject ? selectedProject.includeTasks : []}
-      />
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <DataTable
+          columns={columns}
+          data={selectedProject ? selectedProject.includeTasks : []}
+        />
+      )}
     </div>
   );
 }
