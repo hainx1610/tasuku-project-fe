72a977d269e4554e2df7a1d87e473c9ea9fa8b9e
Harris Nguyen <hainx1610@gmail.com>
2023-10-25 01:06:14 +0700

    chore: reposition spinning loader

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -3,8 +3,8 @@ import { Loader2 } from "lucide-react";
 
 function LoadingScreen() {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <Loader2 className="h-10 w-10 animate-spin" />
+    <div className="w-full h-full flex items-center justify-center ">
+      <Loader2 className="h-10 w-10 animate-spin absolute top-1/2 left-1/2" />
     </div>
   );
 }
