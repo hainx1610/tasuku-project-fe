1edce42d1e6171e2dd991c2f1ab014f2c48f57ae
Harris Nguyen <hainx1610@gmail.com>
2023-10-25 22:48:58 +0700

    chore: add some styling

--- src/layouts/MainFooter.tsx
+++ src/layouts/MainFooter.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MainFooter() {
   return (
-    <div className="py-3 text-xs bg-secondary">
+    <div className="py-3 text-xs bg-secondary z-50">
       {"Designed by "}
       <a href="https://github.com/hainx1610">Harris Nguyen</a>
       {` ${new Date().getFullYear()}.`}
