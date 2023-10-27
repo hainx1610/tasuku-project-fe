48a10734922660558ad612b20151d2244e9415fc
Harris Nguyen <hainx1610@gmail.com>
2023-10-27 14:35:41 +0700

    chore: delete comments

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,23 +3,6 @@ import useAuth from "@/hooks/useAuth";
 function HomePage() {
   const { user } = useAuth();
 
-  // const userId = user!._id;
-
-  // const userRole = user ? user.role : undefined;
-
-  // const dispatch = useDispatch();
-
-  // const { projectsById, currentProjects, isLoading } = useSelector(
-  //   (state) => state.project
-  // );
-
-  // useEffect(() => {
-  //   if (userId) dispatch(getProjectsByUser(userId));
-  //   // no userId the first time
-  // }, [dispatch, userId]);
-
-  // const projects = currentProjects.map((projectId) => projectsById[projectId]);
-
   return (
     <>
       <div className="p-6 space-y-10 my-5 flex flex-col justify-center items-center h-full">
@@ -27,11 +10,10 @@ function HomePage() {
           Good to see you there, {user!.name}.
         </h1>
         <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-          Choose a project from the left menu to view your tasks.
+          Choose a project from the left menu to view your tasks, or create a
+          new one.
         </h4>
       </div>
-
-      {/* <TaskTable /> */}
     </>
   );
 }
