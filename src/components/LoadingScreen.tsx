3df07f362a49a2047a72879a4fba58337a2ab718
Harris Nguyen <hainx1610@gmail.com>
2023-10-28 12:47:47 +0700

    chore: center the loading circle

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from "lucide-react";
 function LoadingScreen() {
   return (
     <div className="w-full h-full flex items-center justify-center ">
-      <Loader2 className="h-10 w-10 animate-spin absolute top-1/2 left-1/2" />
+      <Loader2 className="h-10 w-10 animate-spin absolute top-1/2 " />
     </div>
   );
 }
