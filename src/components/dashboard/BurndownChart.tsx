c155a3873cf171251cca9dfd5387095107b52998
Harris Nguyen <hainx1610@gmail.com>
2023-11-21 22:06:59 +0700

    feat: ideal effort line for burndown chart

--- src/components/dashboard/BurndownChart.tsx
+++ src/components/dashboard/BurndownChart.tsx
@@ -1,3 +1,4 @@
+import { fAddHours, fDateMD, getWeekDayList } from "@/utils/formatTime";
 import {
   Card,
   CardContent,
@@ -6,19 +7,60 @@ import {
   CardTitle,
 } from "../ui/card";
 
-import { Legend, LineChart, ResponsiveContainer } from "recharts";
+import {
+  Area,
+  CartesianGrid,
+  ComposedChart,
+  Legend,
+  Line,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
-function BurndownChart() {
+function BurndownChart({ tasksData, projectData }: any) {
+  const daysList = getWeekDayList(projectData.startAt, projectData.endAt).map(
+    (day) => fDateMD(day)
+  );
+  // add 1 day after end date
+  daysList.push(fDateMD(fAddHours(projectData.endAt, 24)));
+
+  const idealTotalEffortHrs = (daysList.length - 1) * 24;
+  console.log(daysList);
+  const data = daysList.map((day, index) => ({
+    name: day,
+    idealEffort: idealTotalEffortHrs - index * 24,
+  }));
+
   return (
     <Card className=" w-96 m-2">
       <CardHeader>
         <CardTitle>Burndown</CardTitle>
       </CardHeader>
-      <CardContent className="pl-0">
+      <CardContent className="pl-0 pr-3">
         <ResponsiveContainer width={"100%"} height={350}>
-          <LineChart>
+          <ComposedChart width={730} height={250} data={data}>
+            <XAxis dataKey="name" angle={-60} textAnchor="end" fontSize={12} />
+            <YAxis />
+            <Tooltip />
             <Legend />
-          </LineChart>
+            <CartesianGrid stroke="gray" strokeOpacity={0.4} />
+
+            {/* <Area
+              type="monotone"
+              dataKey="amt"
+              fill="#8884d8"
+              stroke="#8884d8"
+            /> */}
+
+            <Line
+              type="monotone"
+              dataKey="idealEffort"
+              name="ideal"
+              stroke="blue"
+            />
+          </ComposedChart>
         </ResponsiveContainer>
       </CardContent>
     </Card>
